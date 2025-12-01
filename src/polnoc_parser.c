39f03d9308070f4daafeb27e49f579198b08e7f9
Ahmed Ali <ahmedcali1984@gmail.com>
2025-12-01 12:24:00 +0300

    [parser] Parse and evaluate the tokens

--- src/polnoc_parser.c
+++ src/polnoc_parser.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 
+#include "./polnoc_lexer.h"
 #include "./polnoc_parser.h"
 
 double plc_parse_number(const char *number_string)
@@ -32,8 +33,65 @@ Plc_Token plc_parser_pop_token(Plc_Tokens *stack)
     return stack->contents[--stack->count];
 }
 
-bool plc_parse_tokens(const Plc_Tokens *tokens)
+bool plc_parse_tokens(const Plc_Tokens *tokens, Plc_Tokens *stack)
 {
-    for (size_t i = 0; i < )
+    memset(stack, 0, sizeof(*stack));
+    for (size_t i = 0; i < tokens->count; ++i) {
+        Plc_Token *token = &tokens->contents[i];
+        switch (token->type) {
+        case PLC_TOKEN_NUMBER: {
+            dyn_array_append(stack, *token);
+        } break;
+
+        case PLC_TOKEN_PLUS: {
+            double right = plc_parser_pop_token(stack).data.number;
+            double left  = plc_parser_pop_token(stack).data.number;
+
+            Plc_Token t = {0};
+            t.data.number = left + right;
+            t.type = PLC_TOKEN_NUMBER;
+            dyn_array_append(stack, t);
+        } break;
+
+        case PLC_TOKEN_MINUS: {
+            double right = plc_parser_pop_token(stack).data.number;
+            double left  = plc_parser_pop_token(stack).data.number;
+
+            Plc_Token t = {0};
+            t.data.number = left - right;
+            t.type = PLC_TOKEN_NUMBER;
+            dyn_array_append(stack, t);
+        } break;
+
+        case PLC_TOKEN_DIV: {
+            double right = plc_parser_pop_token(stack).data.number;
+            double left  = plc_parser_pop_token(stack).data.number;
+            assert(right != 0 && "ERROR: Division by Zero");
+
+            Plc_Token t = {0};
+            t.data.number = left / right;
+            t.type = PLC_TOKEN_NUMBER;
+            dyn_array_append(stack, t);
+        } break;
+
+        case PLC_TOKEN_MULT: {
+            double right = plc_parser_pop_token(stack).data.number;
+            double left  = plc_parser_pop_token(stack).data.number;
+
+            Plc_Token t = {0};
+            t.data.number = left * right;
+            t.type = PLC_TOKEN_NUMBER;
+            dyn_array_append(stack, t);
+        } break;
+
+        case PLC_TOKEN_FUNC: {
+            Plc_Token *top = &stack->contents[0];
+            fprintf(stdout, "INFO: Value: %lf, Type: %s\n", top->data.number, plc_token_type_as_cstr(top->type));
+        } break;
+
+        default:
+            assert(0 && "unreachable token type");
+        }
+    }
     return true;
 }
